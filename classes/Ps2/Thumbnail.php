8880d88132799b580cf4d906479a9ef9af858a5e
Peng Zhou <statchou@gmail.com>
2014-03-28 18:20:23 -0400

    8-2 Creating the setter methods

--- classes/Ps2/Thumbnail.php
+++ classes/Ps2/Thumbnail.php
@@ -1,5 +1,7 @@
 <?php
-class Ps2_Thumbnail {
+
+class Ps2_Thumbnail
+{
     protected $_original;
     protected $_originalwidth;
     protected $_originalheight;
@@ -13,7 +15,8 @@ class Ps2_Thumbnail {
     protected $_suffix = '_thb';
     protected $_messages = array();
 
-    public function __construct($image) {
+    public function __construct($image)
+    {
         if (is_file($image) && is_readable($image)) {
             $details = getimagesize($image);
         } else {
@@ -32,7 +35,8 @@ class Ps2_Thumbnail {
         }
     }
 
-    protected function checkType($mime) {
+    protected function checkType($mime)
+    {
         $mimetypes = array('image/jpeg', 'image/png', 'image/gif');
         if (in_array($mime, $mimetypes)) {
             $this->_canProcess = true;
@@ -41,11 +45,53 @@ class Ps2_Thumbnail {
         }
     }
 
-    public function test() {
+    public function setDestination($destination)
+    {
+        if (is_dir($destination) && is_writable($destination)) {
+            // get last character
+            $last = substr($destination, -1);
+            // add a trailing slash if missing
+            if ($last == '/' || $last == '\\') {
+                $this->_destination = $destination;
+            } else {
+                $this->_destination = $destination . DIRECTORY_SEPARATOR;
+            }
+        } else {
+            $this->_messages[] = "Cannot write to $destination.";
+        }
+    }
+
+    public function setMaxSize($size) {
+        if (is_numeric($size) && $size>0) {
+            $this->_maxSize = $size;
+        } else {
+            $this->_message[] = 'The value for setMaxSize() must be a positive number.';
+            $this->_canProcess = false;
+        }
+    }
+
+    public function setSuffix($suffix)
+    {
+        if (preg_match('/^\w+$/', $suffix)) {
+            if (strpos($suffix, '_') !== 0) {
+                $this->_suffix = '_' . $suffix;
+            } else {
+                $this->_suffix = $suffix;
+            }
+        } else {
+            $this->_suffix = '';
+        }
+    }
+
+    public function test()
+    {
         echo 'File: ' . $this->_original . '<br>';
         echo 'Original width: ' . $this->_originalwidth . '<br>';
         echo 'Original height: ' . $this->_originalheight . '<br>';
         echo 'Image type: ' . $this->_imageType . '<br>';
+        echo 'Destination: ' . $this->_destination . '<br>';
+        echo 'Max size: ' . $this->_maxSize . '<br>';
+        echo 'Suffix: ' . $this->_suffix . '<br>';
         if ($this->_messages) {
             print_r($this->_messages);
         }
--- gd/create_thumb_mac.php
+++ gd/create_thumb_mac.php
@@ -1,8 +1,17 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 if (isset($_POST['create'])) {
     require_once('../classes/Ps2/Thumbnail.php');
     try {
         $thumb = new Ps2_Thumbnail($_POST['pix']);
+        if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] =='PhpStorm 7.1') {
+            $thumb->setDestination('/Users/pengzhou/upload_test/thumbs/');
+        } else if ($_SERVER['SERVER_NAME'] == 'codepeng.com' || $_SERVER['SERVER_NAME'] == 'www.codepeng.com') {
+            $thumb->setDestination('/home/users/web/b1060/ipg.codepengcom/upload_test/thumbs');
+        }
+        $thumb->setMaxSize(100);
+        $thumb->setSuffix('small');
         $thumb->test();
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -12,27 +21,31 @@ if (isset($_POST['create'])) {
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Create Thumbnail</title>
+    <meta charset="utf-8">
+    <title>Create Thumbnail</title>
 </head>
 
 <body>
-  <form id="form1" name="form1" method="post" action="">
+<form id="form1" name="form1" method="post" action="">
     <p>
-      <select name="pix" id="pix">
-        <option value="">Select an image</option>
-        <?php
-	$files = new DirectoryIterator('../images');
-	$images = new RegexIterator($files, '/\.(?:jpg|png|gif)$/i');
-	foreach ($images as $image) {
-	?>
-        <option value="/Applications/MAMP/htdocs/Test/images/<?php echo $image; ?>"><?php echo $image; ?></option>
-        <?php } ?>
-      </select>
+        <select name="pix" id="pix">
+            <option value="">Select an image</option>
+            <?php
+            $files = new DirectoryIterator('../images');
+            $images = new RegexIterator($files, '/\.(?:jpg|png|gif)$/i');
+            if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] =='PhpStorm 7.1') {
+                foreach ($images as $image) { ?>
+                    <option value="/Applications/MAMP/htdocs/Test/images/<?php echo $image; ?>"><?php echo $image; ?></option>
+                <?php } } else if ($_SERVER['SERVER_NAME'] == 'codepeng.com' || $_SERVER['SERVER_NAME'] == 'www.codepeng.com') {
+                foreach ($images as $image) { ?>
+                    <option value="/home/users/web/b1060/ipg.codepengcom/Test/images/<?php echo $image; ?>"><?php echo $image; ?></option>
+                <?php
+                } }?>
+        </select>
     </p>
     <p>
-      <input type="submit" name="create" id="create" value="Create Thumbnail">
+        <input type="submit" name="create" id="create" value="Create Thumbnail">
     </p>
-  </form>
+</form>
 </body>
 </html>
