a70bfedb8406d958689580dcd2377b14e21bde55
Peng Zhou <statchou@gmail.com>
2014-04-06 10:50:36 -0400

    11 -4 -5: Displaying the images table using -MySQLi -PDO

--- mysql/mysqli.php
+++ mysql/mysqli.php
@@ -21,5 +21,19 @@ $numRows = $result->num_rows;
 
 <body>
 <p>A total of <?php echo $numRows; ?> records were found.</p>
+<table>
+    <tr>
+        <th>image_id</th>
+        <th>filename</th>
+        <th>caption</th>
+    </tr>
+    <?php while ($row = $result->fetch_assoc()) { ?>
+        <tr>
+            <td><?php echo $row['image_id']; ?></td>
+            <td><?php echo $row['filename']; ?></td>
+            <td><?php echo $row['caption']; ?></td>
+        </tr>
+    <?php } ?>
+</table>
 </body>
 </html>
--- mysql/pdo.php
+++ mysql/pdo.php
@@ -20,5 +20,19 @@ $numRows = $result->rowCount();
 
 <body>
 <p>A total of <?php echo $numRows; ?> records were found.</p>
+<table>
+    <tr>
+        <th>image_id</th>
+        <th>filename</th>
+        <th>caption</th>
+    </tr>
+    <?php foreach ($conn->query($sql) as $row) { ?>
+        <tr>
+            <td><?php echo $row['image_id']; ?></td>
+            <td><?php echo $row['filename']; ?></td>
+            <td><?php echo $row['caption']; ?></td>
+        </tr>
+    <?php } ?>
+</table>
 </body>
 </html>
